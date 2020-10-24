4381954fb655e5003ca92cd1badfb4151fb678f1
jenfer <jfernandez.ph@gmail.com>
2020-10-25 02:39:06 +0800

    updated with US volcanoes mapping

--- map.py
+++ map.py
@@ -1,6 +1,30 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+nam = list(data["NAME"])
+typ = list(data["TYPE"])
+lat = list(data["LAT"])
+lon = list(data["LON"])
+ele = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Name: %s <br>
+Type: %s <br>
+Elevation: %s
+"""
+
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Marker here", icon=folium.Icon(color="green")))
+fg = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, nm, tp, el in zip(lat, lon, nam, typ, ele):
+    # marker = f"Name:{nm}\nElevation:{el}"
+    iframe = folium.IFrame(html=html % (nm, tp, el), width=200, height=150)
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe), icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
 
 map.save("map.html")
